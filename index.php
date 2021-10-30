85086fcc7b0840414b45a660ebde0e840e691335
Killbots2012 <turpel_123@live.com>
2021-10-30 19:24:29 -0300

    Fixed the repo

---
+++ index.php
@@ -0,0 +1,172 @@
+<!DOCTYPE html>
+<html lang="en">
+        <meta charset="utf-8">
+        <meta name="viewpoint" content="with=device-width, initial-scale=1.0">
+        <meta name="description" content="">
+        <meta name="author" content="Killbots2012">
+        <title>ModernMC</title>
+        <link rel="ICON" href="assets/img/server-icon.png">
+        <!-- Bootstrap core CSS -->
+        <link rel="stylesheet" href="assets/bootstrap/bootstrap.css">
+
+        <!-- ModernMC CSS -->
+        <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="fork-awesome/css/fork-awesome.min.css">
+        <!-- FontAwesome CSS -->
+
+    </head>
+
+    <body style="background-color:h hotpink;">
+        <nav class="nav navbar navbar-expand-xxl navbar-dark bg-dark ">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="index.php">ModernMC</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="true" href="index.php"><i class="fa fa-home"></i> Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="pages/rules.html"><i class=" fa fa-info-circle"></i>  Rules</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="pages/guide.html"><i class="fa fa-question-circle"></i>  Guides</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="pages/vote.html"><i class="fa fa-check"></i>  Vote</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="pages/staff.html"><i class=" fa fa-star"></i> Staff</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="https://modermmc.enjin.com/shop"><i class="fa fa-shopping-cart"></i> Shop</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="https://staff.modernmc.ca/"><i class="fa fa-gear"></i> Admin</a>
+                        </li>
+                    </ul>
+                    <form class="d-flex">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+        <!-- Big opening-->
+<style>
+    .homepage-opening {
+  /* margin-top: 20px; */
+  margin-bottom: 100px;
+  background-image: url("assets/img/cathidral.png");
+  padding: 20px, 20px, 20px;
+  background-position: center center;
+  text-shadow: 2px 2px rgba(0,0,0,.4);
+  background-repeat:none;
+  color: #1CC7E3;
+    }
+</style>
+        <div class="jumbotron homepage-opening">
+            <div class="container-fluid">
+                <div class="row text-center">
+                    <!-- <h1 class="head" >ModernMC</h1> -->
+                    <header class="head">ModernMC</header>
+                    <h1 class="leader">1.16.5: play.modernmc.ca</h1>
+                    <h2 class="leader">Snapshots: beta.modernmc.ca</h2>
+                    <br />
+                    <center><a class="btn btn-huge btn-info btn-glow" href="https://www.minecraft.net/en-us/download">Download Minecraft!</a></center>
+                </div>
+            </div>
+        </div>
+        <!-- /Big opening -->
+
+        <div class="">
+            <div class="row text-center mc-items">
+                <div class="col-md-4">
+                    <div class="icon">
+
+                        <a href="/guides/towny-guide.html"><img src="assets/img/towny.jpeg" sizes="100px" /></a>
+                    </div>
+                    <h2 class="leader">Survival</h2>
+                    <p>Survival Minecraft is a game play where you need to collect all your resources to build, and survive from monsters
+                        <br />
+                        <br />
+                    </p>
+                </div>
+                <div class="col-md-4">
+                    <div class="icon">
+                        <img src="assets/img/chest.png" />
+                    </div>
+                    <h2 class="leader">Creative</h2>
+                    <p>The Creative server is a place to test your build ideas out, before you attempt it in Survival Mode or on the Towny Server
+                        <br />
+                        <br />
+                    </p>
+                </div>
+                <div class="col-md-4">
+                    <div class="icon">
+                        <a href="towny-guide.html"><img src="assets/img/diamond.png" /></a>
+                    </div>
+                    <h2 class="leader">Towny</h2>
+                    <p>The original Player-managed Protection within a resident/town/nation-hierarchy, is also the place to hang out collect MCmmo points, and hang out with people.
+                        <br />
+                        <br />
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="container-fluid">
+            <div class="container">
+                <h2 class="container">News</h2>
+                <hr>
+                <h3 style="font-family: Arial, Helvetica, sans-serif;">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus quis omnis est facilis quos cupiditate velit ullam illum earum iusto tempora animi aliquam alias ut soluta porro, magnam labore ex.
+                </h3>
+            </div>
+
+        </div>
+        <!-- /.container -->
+        <div class="container">
+
+            <hr>
+            <footer class="social-footer">
+                <nav class="navbar navbar-expand-sm navbar-light xxl-dark">
+                    <div class="container-fluid">
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent" style="margin-block-start: 40px;">
+                            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                                <li class="nav-item">
+                                    <a class="nav-link" style="color: aquamarine;" href="http://discord.modernmc.ca"><i class="fa fa-discord"></i></a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" style="color: aquamarine;" href="http://facebook.modernmc.ca/"><i class=" fa fa-facebook"></i></a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" style="color: aquamarine;" href="http://instagram.modernmc.ca"><i class="fa fa-instagram"></i></a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" style="color: aquamarine;" href="http://twitter.modernmc.ca"><i class="fa fa-twitter"></i></a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" style="color: aquamarine;" href="http://shop.modernmc.ca"><i class="fa fa-shopping-cart"></i></a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" style="color: aquamarine;" href="https://staff.modernmc.ca/"><i class="fa fa-gear"></i></a>
+                                </li>
+                        </div>
+                    </div>
+                </nav>
+            </footer>
+            </div>
+        </hr>
+        </div>
+        <!-- /.container -->
+        <!-- JavaScript -->
+        <script src="assets/js/jquery-1.10.2.js"></script>
+        <script src="assets/js/bootstrap.js"></script>
+    </body>
+
+</html>
---
+++ todo.md
@@ -0,0 +1,13 @@
+
+<h1>create guides for</h1>
+<b>Towny</b>
+<ul>Towny it's self,</ul> 
+<ul>mcmmo,</ul> 
+<ul>jobs,</ul> 
+<ul>player shops,</ul> 
+<ul>money making guide.</ul>
+
+<b>Creative</b>
+<ul>Guide on plot squared</ul>
+
+<h3>fix social icons to proper scale</h3>
